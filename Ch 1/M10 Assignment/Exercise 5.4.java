79cba51d4b7e0581e0b1f833f48fab5b290f26c0
brodpuck <brodpuck@iu.edu>
2025-11-17 22:04:04 -0500

    Add files via upload

---
+++ Ch 1/M10 Assignment/Exercise 5.4.java
@@ -0,0 +1,10 @@
+public class Exercise05_04 {
+    public static void main(String[] args) {
+        System.out.println("Miles\t\tKilometers");
+         int miles = 1; 
+        while (miles <= 10) {
+            System.out.println(miles + "\t\t" + miles * 1.609);
+            miles++;
+        }
+    }
+}
---
+++ Ch 1/M10 Assignment/Exercise 5.6.java
@@ -0,0 +1,18 @@
+public class Exercise05_06 {
+  public static void main(String[] args) {
+    System.out.printf("%10s%10s    |  %10s%10s\n", "Miles  ", "Kilometers",
+        "Kilometers", "Miles");
+    System.out.println("---------------------------------------------");
+
+    int miles = 1;
+    int kilometers = 20;
+    int count = 1;
+    while (count <= 10) {
+      System.out.printf("%10d%10.3f    |  %10d%10.3f\n", miles, miles * 1.609,
+          kilometers, kilometers / 1.609);
+      miles++;
+      kilometers += 5;
+      count++;
+    }
+  }
+}
